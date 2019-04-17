947d8316734ba19e6135dac99dd15a54ad4c84fb
Kamil Sztefko <sztefkokamil@gmail.com>
2019-04-17 12:16:44 +0200

    addWorker panel & deleteWorker buttons ADD

--- src/components/modals/workersList.vue
+++ src/components/modals/workersList.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="workers-list-modal">
     <button class='close-modal-btn' @click="$store.dispatch('hideModal')">X</button>
+    <div class="add-worker">
+      <button @click="addWorkerSwitch">{{addWorkerText}}</button>
+      <form @submit.prevent="addWorker" class="add-worker--form" v-if="addWorkerOn">
+        <label for="worker-firstname">imię <input id="worker-firstname" type="text" v-model="wFirstname" required></label>
+        <label for="worker-lastname">nazwisko <input id="worker-lastname" type="text" v-model="wLastname" required></label>
+        <label for="worker-pesel">pesel <input id="worker-pesel" type="text" v-model="wPesel" required></label>
+        <label for="worker-email">email <input id="worker-email" type="email" v-model="wEmail" required></label>
+        <button type="submit">Dodaj</button>
+      </form>
+    </div>
     <form class="search-worker--form">
       <label for="search-worker--input">Wpisz imię i nazwisko</label>
       <input type="text" id="search-worker--input" v-model="workerName">
     </form>
-    <ul>
+    <ul class="workers-list">
       <li v-for="worker in workers" :key="worker.pesel">
-        {{worker.firstname}} {{worker.lastname}}, pesel: {{worker.pesel}}, email: {{worker.email}}
+        <span>{{worker.firstname}} {{worker.lastname}}, pesel: {{worker.pesel}}, email: {{worker.email}}</span>
+        <button @click="deleteWorker" :data-id="worker.id">usuń</button>
       </li>
     </ul>
   </div>
@@ -18,7 +29,40 @@ export default {
   data(){
     return {
       workers: this.$store.getters.getWorkers,
-      workerName: ''
+      workerName: '',
+      addWorkerOn: false,
+      addWorkerText: 'Dodaj pracownika',
+      wFirstname: '',
+      wLastname: '',
+      wPesel: '',
+      wEmail: ''
+    }
+  },
+  methods: {
+    addWorkerSwitch(){
+      this.addWorkerOn = !this.addWorkerOn;
+      if(this.addWorkerOn){
+        this.addWorkerText = 'Zamknij panel';
+      }
+      else{
+        this.addWorkerText = 'Dodaj pracownika';
+      }
+    },
+    addWorker(){
+      const newWorker = {
+        firstname: this.wFirstname,
+        lastname: this.wLastname,
+        pesel: this.wPesel,
+        email: this.wEmail
+      };
+
+      console.log(newWorker);
+
+      // fetchWorkers
+    },
+    deleteWorker(e){
+      const workerId = e.target.attributes['data-id'].value;
+      console.log(`delete worker: id ${workerId}`);
     }
   },
   watch: {
@@ -39,13 +83,14 @@ export default {
 @import "../../sass/flexMixins.scss";
 
 .workers-list-modal{
-  @include flexColumn;
+  @include flexColumn(start, center);
   position: fixed;
   left: 0;
   top: 0;
   z-index: 10;
   width: 100vw;
   height: 100vh;
+  padding-top: 50px;
   background: gray;
 
   .close-modal-btn{
@@ -55,13 +100,31 @@ export default {
     right: 10px;
   }
 
+  .add-worker{
+    @include flexColumn;
+    margin-bottom: 30px;
+
+    .add-worker--form{
+      @include flexColumn;
+    }
+  }
+
   .search-worker--form{
     @include flexColumn;
     margin-bottom: 20px;
   }
 
-  li{
-    margin-bottom: 10px;
+  .workers-list{
+
+
+    li{
+      margin-bottom: 10px;
+
+      button{
+        padding: 1px 6px;
+        margin-left: 20px;
+      }
+    }
   }
 
 }
