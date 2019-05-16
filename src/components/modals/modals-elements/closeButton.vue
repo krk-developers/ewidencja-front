fd9ec71005122180056beab7dce3059cb9d04f2c
Kamil Sztefko <sztefkokamil@gmail.com>
2019-05-16 21:39:59 +0200

    Fix modal close button position bug
    Position changed from fixed to absolute.

--- src/components/modals/modals-elements/closeButton.vue
+++ src/components/modals/modals-elements/closeButton.vue
@@ -8,9 +8,9 @@
 .close-modal-btn{
   @include buttonWhite;
   padding: 5px 15px;
-  position: fixed;
+  position: absolute;
   top: 15px;
-  right: 15px;
+  right: 20px;
   font-size: 24px;
   font-weight: 600;
 }
