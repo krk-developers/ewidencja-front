81c0a0a08e3b560147933fe1042dac78fbbee7e4
Kamil Sztefko <sztefkokamil@gmail.com>
2019-05-21 08:52:18 +0200

    Improve closeButton styles
    Changed line-height to 100%.
    Changed padding top/bottom to 7px.

--- src/components/modals/modals-elements/closeButton.vue
+++ src/components/modals/modals-elements/closeButton.vue
@@ -4,14 +4,16 @@
 
 <style lang="scss">
 @import "../../../sass/elements.scss";
+@import "../../../sass/flexMixins.scss";
 
 .close-modal-btn{
   @include buttonWhite;
-  padding: 5px 15px;
+  padding: 7px 15px;
   position: absolute;
   top: 15px;
   right: 20px;
   font-size: 24px;
   font-weight: 600;
+  line-height: 100%;
 }
 </style>
