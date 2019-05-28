dd8e094e510b5abeda2bbd4b66cfeaa46481422e
Kamil Sztefko <sztefkokamil@gmail.com>
2019-05-28 08:19:05 +0200

    Rework addWorkerForm
    Removed old & useless code.
    Uncommented addWorker & editWorker VUEX dispatches.

--- src/components/modals/modals-elements/addWorkerForm.vue
+++ src/components/modals/modals-elements/addWorkerForm.vue
@@ -5,16 +5,6 @@
       <label for="worker-lastname">nazwisko <input id="worker-lastname" type="text" v-model="worker.lastname" placeholder="nazwisko" title="nazwisko" required></label>
       <label for="worker-pesel">PESEL <input id="worker-pesel" type="number"  v-model="worker.pesel" placeholder="11 cyfer" title="pesel musi mieć 11 cyfer" required></label>
       <label for="worker-email">email <input id="worker-email" type="email" v-model="worker.email" placeholder="przykład@test.pl" title="przykład@test.pl" required></label>
-      <!-- <label for="worker-contractFrom">umowa od <input id="worker-contractFrom" type="text" v-model="worker.contractFrom" placeholder="RRRR-MM-DD" title="data w formacie RRRR-MM-DD - np. 2012-12-30"></label>
-      <label for="worker-contractTo">umowa do <input id="worker-contractTo" type="text" v-model="worker.contractTo" placeholder="RRRR-MM-DD" title="data w formacie RRRR-MM-DD - np. 2012-12-30"></label>
-      <label for="worker-partTime">wymiar etatu
-        <select name="partTime" id="worker-partTime" v-model="worker.partTime" title="wymiar etatu pracownika">
-          <option value="1" selected>1</option>
-          <option value="0.75">0.75</option>
-          <option value="0.5">0.5</option>
-          <option value="0.25">0.25</option>
-        </select>
-      </label> -->
       <label for="worker-effective">liczba efektywna
         <select name="effective" id="worker-effective" v-model="worker.effective" title="liczba efektywna pracownika">
           <option value="1" selected>1</option>
@@ -41,9 +31,6 @@ export default {
         lastname: '',
         pesel: '',
         email: '',
-        // contractFrom: '',
-        // contractTo: '',
-        // partTime: '1',
         equivalent: '',
         effective: ''
       },
@@ -58,11 +45,6 @@ export default {
         pesel: this.worker.pesel,
         name: this.worker.name,
         email: this.worker.email,
-        // contract_from: this.worker.contractFrom,
-        // contract_to: this.worker.contractTo,
-        // part_time: this.worker.partTime,
-        // equivalent: '0',
-        // equivalent_amount: '0',
         effective: this.worker.effective,
       }
 
@@ -75,20 +57,16 @@ export default {
         this.newWorker.equivalent_amount = this.worker.equivalent;
       }
 
-      // if(this.newWorker.contract_from === ''){
-      //   this.newWorker.contract_from = '2001-01-01';
-      // }
-
       // edit worker
       if(this.workerData.pesel){
         this.newWorker.id = this.workerData.id;
-        // this.$store.dispatch('editWorker', this.newWorker);
+        this.$store.dispatch('editWorker', this.newWorker);
       }
       // add worker
       else{
         this.newWorker.password = "12345678";
         this.newWorker.password_confirmation = "12345678";
-        // this.$store.dispatch('addWorker', this.newWorker);
+        this.$store.dispatch('addWorker', this.newWorker);
       }
 
     },
@@ -99,15 +77,6 @@ export default {
         this.worker.pesel = this.workerData.pesel;
         this.worker.email = this.workerData.user.email;
 
-        // if(this.workerData.contract_from !== '2001-01-01'){
-        //   this.worker.contractFrom = this.workerData.contract_from;
-        // }
-        // if(this.workerData.contract_to){
-        //   this.worker.contractTo = this.workerData.contract_to;
-        // }
-        // if(this.workerData.part_time){
-        //   this.worker.partTime = this.workerData.part_time;
-        // }
         if(!this.workerData.equivalent_amount || this.workerData.equivalent_amount === '0.00'){
           this.worker.equivalent = '0';
         }
